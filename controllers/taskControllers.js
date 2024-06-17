cb454b8dc27ced951003f045da3e64bfbaf22791
mrshuzaifa <qanitahuzaifa@gmail.com>
2024-06-18 03:33:06 +0500

    deployment commit

---
+++ controllers/taskControllers.js
@@ -0,0 +1,63 @@
+const asyncHandler = require('express-async-handler');
+const Task = require('../models/taskModel');
+const User = require('../models/userModel');
+
+const getTasks = asyncHandler (async (req, res) => {
+    let tasks = await Task.find( { user : req.user.id } );
+    if(!tasks)
+        throw new Error('No task found by this user');
+    res.status(200).json(tasks);
+});
+
+const setTask = asyncHandler (async (req, res) => {
+    if(!req.body.text){
+        res.status(400);
+        throw new Error('Please enter a task' );
+    }
+    const task = await Task.create({ text: req.body.text,  user : req.user.id })
+    res.status(200).json(task);
+});
+
+const updateTask = asyncHandler (async (req, res) => {
+    const user = await User.findById(req.user.id);
+    if(!user){
+        res.status(401);
+        throw new Error('No such user found');
+    }
+    let task = await Task.findById(req.params.id);
+    if (!task) {
+        res.status(400);
+        throw new Error('Task not found');
+    }
+    if (task.user.toString() !== user.id) {
+        res.status(401);
+        throw new Error('User is not authorized to update');
+    }
+    const updatedTask = await Task.findByIdAndUpdate (
+        req.params.id, 
+        req.body,   
+        { new: true }
+    );
+    res.status(200).json(updatedTask);
+});
+
+const deleteTask = asyncHandler (async (req, res) => {
+    const user = await User.findById(req.user.id);
+    if(!user){
+        res.status(401);
+        throw new Error('No such user found');
+    }
+    let task = await Task.findById(req.params.id);
+    if (!task) {
+        res.status(400)
+        throw new Error('Task not found')
+    }
+    if (task.user.toString() !== user.id) {
+        res.status(401);
+        throw new Error('User is not authorized to delete');
+    }
+    await Task.findByIdAndDelete(req.params.id);
+    res.status(200).json({ id: req.params.id, message : 'Deleted' });
+});
+
+module.exports = {getTasks, setTask, updateTask, deleteTask};
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const cors = require('cors');
+const PORT = process.env.PORT || 5000;
+const {errorHandler} =  require('./middleware/errorMiddleware');
+const connectToDatabase = require('./connect/database');
+connectToDatabase();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended :  false}));
+app.use(cors());
+app.use('/api/tasks', require('./routes/taskRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+app.use(errorHandler);
+app.listen(PORT, ()=>console.log(`Server listening on ${PORT}`));
